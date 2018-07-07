3cf8b0fe8361b06393697aabd571e7e8fabbe3d5
Nathan Glenn <nathan-gary.glenn@trivago.com>
2018-07-07 14:33:23 +0200

    Add capability to add todos, basic DOM updating

--- script.js
+++ script.js
@@ -9,6 +9,35 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+const state = {
+    itemCount: 0,
+    uncheckedCount: 0,
+    todos: []
+}
+
 function newTodo() {
-  alert('New TODO button clicked!')
+    var todo = prompt("What do you want to do?", "Your TODO here");
+    if(todo != null) {
+        addTodo(todo)
+        updateDOM()
+    }
+}
+
+function addTodo(todo) {
+    state.todos.push(todo)
+    state.itemCount++
+    state.uncheckedCount++
+}
+
+function updateDOM() {
+    list.innerHTML = ''
+    state.todos.forEach((todo) => {
+        list.innerHTML += getTODOHtml(todo)
+    });
+    itemCountSpan.innerHTML = state.itemCount
+    uncheckedCountSpan.innerHTML = state.uncheckedCount
+}
+
+function getTODOHtml (todo) {
+    return '<li>' + todo + '</li>'
 }
