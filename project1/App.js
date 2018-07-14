74f591caf0b7ecd2c0f806e1abf1b314dfe22466
Nathan Glenn <nathan-gary.glenn@trivago.com>
2018-07-14 19:09:19 +0200

    Basic timer working

--- project1/App.js
+++ project1/App.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {PropTypes} from 'prop-types';
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isWork: true,
+      isTimerRunning: false,
+      timeLeft: this.props.secondsPerWork,
+    }
+    setInterval(this.decrementSeconds, 1000)
+  }
+
+  decrementSeconds = () => {
+    this.setState({
+      ...this.state,
+      'timeLeft': this.state.timeLeft - 1
+    })//, checkTime
+  }
+
+  getTimeString = (seconds) => {
+    return seconds
+  }
+
+  render() {
+    return (
+      <Text>{this.getTimeString(this.state.timeLeft)}</Text>
+    )
+  }
+
+  static propTypes = {
+    secondsPerWork: PropTypes.integer,
+    secondsPerRest: PropTypes.integer,
+  }
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up Ap.js to start working on your app!</Text>
+        <Timer secondsPerWork={60} secondsPerRest={60}></Timer>
       </View>
     );
   }
